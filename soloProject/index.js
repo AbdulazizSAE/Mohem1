e56d3848d6399348b686f52b25c52792169eafa7
AbdulazizSAE <codesaeed@gmail.com>
2023-08-30 17:11:26 +0300

    Initial commit

---
+++ soloProject/index.js
@@ -0,0 +1,4 @@
+import { initializeApp } from "https://playground-6f919-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
