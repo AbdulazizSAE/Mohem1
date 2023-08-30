3f44096708ffd568d17665dd6cebbc59e1708a0c
AbdulazizSAE <codesaeed@gmail.com>
2023-08-31 00:39:01 +0300

    semi final version

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
-import { initializeApp } from "https://playground-6f919-default-rtdb.asia-southeast1.firebasedatabase.app/"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://mohem-d0e42-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const missionsInDB = ref(database, "missions")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const listsEl = document.getElementById("lists")
+
+addButtonEl.addEventListener("click", function(){
+
+    let inputValue = inputFieldEl.value
+    push(missionsInDB, inputValue)
+
+    clearInputFieldEl()
+
+})
+
+onValue(missionsInDB, function(snapshot){
+    let missionArray = Object.values(snapshot.val())
+
+    clearListsEl()
+
+    for(let i=0;i<missionArray.length;i++){
+        let currentMission = missionArray[i]
+        let currentMissionID = currentMission[0]
+        let currentMissionValue = currentMission[1]
+        appendItemToList(currentMission)
+    }
+})
+function clearListsEl(){
+    listsEl.innerHTML = ""
+}
+function clearInputFieldEl(){
+    inputFieldEl.value = ""
+}
+
+function appendItemToList(mission){
+    let missionID=mission[0]
+    let missionValue=mission[1]
+
+    let newEl = document.createElement("li")
+    newEl.textContent = mission
+
+    newEl.addEventListener("click", function(){
+        let exactLocationOfMissionInDB = ref(database, `missions/${missionID}`)
+
+        remove(exactLocationOfMissionInDB)
+    })
+
+    listsEl.append(newEl)
+}
